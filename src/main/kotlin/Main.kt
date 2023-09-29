ebd288ce73698959579e4c2afe5ac9ae60fc58d3
Djoufson CHE BENE <djouflegran@gmail.com>
2023-09-29 11:09:44 +0100

    section-3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,18 +17,56 @@ fun main() {
     // printHello()
 
     // 2. Learn why (almost) everything has a value
-    val isUnit = println("This is an expression")
-    println(isUnit)
+    // val isUnit = println("This is an expression")
+    // println(isUnit)
 
-    val temperature = Random().nextInt(0,100)
-    val isHot = temperature > 50
-    val fistMessage = if(isHot) "$temperature is Hot" else "$temperature is Cold"
+    // val temperature = Random().nextInt(0,100)
+    // val isHot = temperature > 50
+    // val fistMessage = if(isHot) "$temperature is Hot" else "$temperature is Cold"
 
-    val secondMessage = "The water temperature is ${ if(isHot) "too warm" else "OK" }"
-    println(fistMessage)
-    println(secondMessage)
+    // val secondMessage = "The water temperature is ${ if(isHot) "too warm" else "OK" }"
+    // println(fistMessage)
+    // println(secondMessage)
+
+    // 3. Learn more about functions
+    feedFish()
+
 }
 //
 //fun printHello(){
 //    println("Hello")
 //}
+
+fun feedFish(){
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eats $food")
+}
+
+fun randomDay() : String{
+    val days = arrayOf(
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday")
+
+    val randomIndex = Random().nextInt(days.size)
+    return days[randomIndex]
+}
+
+fun fishFood(day: String) : String{
+    var food = ""
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Tuesday" -> food = "pellets"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Saturday" -> food = "lettuce"
+        "Sunday" -> food = "plankton"
+    }
+    return food
+}
