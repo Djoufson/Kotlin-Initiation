81701fc201b58b0897fe3bf44a82ae7dd1e87915
Djoufson CHE BENE <djouflegran@gmail.com>
2023-09-29 10:52:17 +0100

    section-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.util.Random
+
 /*
 * TP 1 d'Atelier de dev mobile
 * CHE BENE 19G00266
@@ -12,9 +14,21 @@
 
 fun main() {
     // 1. Explore the main function
-    printHello()
-}
+    // printHello()
 
-fun printHello(){
-    println("Hello")
+    // 2. Learn why (almost) everything has a value
+    val isUnit = println("This is an expression")
+    println(isUnit)
+
+    val temperature = Random().nextInt(0,100)
+    val isHot = temperature > 50
+    val fistMessage = if(isHot) "$temperature is Hot" else "$temperature is Cold"
+
+    val secondMessage = "The water temperature is ${ if(isHot) "too warm" else "OK" }"
+    println(fistMessage)
+    println(secondMessage)
 }
+//
+//fun printHello(){
+//    println("Hello")
+//}
