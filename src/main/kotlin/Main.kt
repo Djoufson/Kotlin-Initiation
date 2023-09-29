4001ea759c5fe8767f1232864c7a17c999779620
Djoufson CHE BENE <djouflegran@gmail.com>
2023-09-29 10:43:14 +0100

    section-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
+/*
+* TP 1 d'Atelier de dev mobile
+* CHE BENE 19G00266
+* NAGA DANIELLE 17G
+* ---
+* Pour assurer que le code est correct, on va commenter
+* le code de chaque partie.
+* Il faudra dé-commenter pour tester.
+*
+* Ou alors visiter le commit correspondant a la partie en question et tester
+* */
+
 fun main() {
-    
+    // 1. Explore the main function
+    printHello()
+}
+
+fun printHello(){
+    println("Hello")
 }
