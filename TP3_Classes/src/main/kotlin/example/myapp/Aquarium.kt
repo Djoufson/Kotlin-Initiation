6da321b2cf83f77fad63559e056f284dae299c68
Djoufson CHE BENE <djouflegran@gmail.com>
2023-10-03 14:17:27 +0100

    Configuration de la classe Aquarium avec son constructeur

--- TP3_Classes/src/main/kotlin/example/myapp/Aquarium.kt
+++ TP3_Classes/src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,11 +1,9 @@
 package example.myapp
 
-class Aquarium (){
-    private var width: Int = 20
-    private var height: Int = 40
-    private var length: Int = 100
-
-
+class Aquarium (
+    private var width: Int = 10,
+    private var height: Int = 20,
+    private var length: Int = 30){
 
     fun printSize(){
         println("Width: $width cm\nHeight: $height cm\nLength: $length cm")
