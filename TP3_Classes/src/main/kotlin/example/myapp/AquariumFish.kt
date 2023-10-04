6f7420892d3565f7f11c49558adf0ac0478a05e6
Djoufson CHE BENE <djouflegran@gmail.com>
2023-10-04 16:37:24 +0100

    Compare abstract classes and interfaces

---
+++ TP3_Classes/src/main/kotlin/example/myapp/AquariumFish.kt
@@ -0,0 +1,23 @@
+package example.myapp
+
+abstract class AquariumFish{
+    abstract val color: String
+}
+
+class Shark: AquariumFish(), FishAction {
+    override val color: String = "gray"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus: AquariumFish(), FishAction{
+    override val color: String = "gold"
+    override fun eat() {
+        println("eat algae")
+    }
+}
+
+interface FishAction{
+    fun eat()
+}
--- TP3_Classes/src/main/kotlin/example/myapp/Main.kt
+++ TP3_Classes/src/main/kotlin/example/myapp/Main.kt
@@ -7,7 +7,18 @@ package example.myapp
 * */
 
 fun main(){
-    buildAquarium()
+//    buildAquarium()
+    makeFish()
+}
+
+fun makeFish(){
+    val shark = Shark()
+    val pleco = Plecostomus()
+
+    println("Shark: ${ shark.color }")
+    shark.eat()
+    println("Plecostomus: ${ pleco.color }")
+    pleco.eat()
 }
 
 fun buildAquarium(){
